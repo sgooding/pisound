de470adf24353c6d12c16b93c6470a65aa4256ff
Sean <sgooding@gmail.com>
2022-08-21 17:36:20 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pisound',
+      version='1.0',
+      description='Raspberry pi sound board',
+      author='Sean Gooding',
+      author_email='sgooding@gmail.com',
+      url='https://github.com/sgooding/pisound',
+      packages=['pisound'],
+     )
