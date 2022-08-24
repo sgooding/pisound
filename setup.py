dcf27034d77550ef9da6ae3074bfab7730bbe36c
Sean <sgooding@gmail.com>
2022-08-24 07:31:19 -0600

    install_requires flags

--- setup.py
+++ setup.py
@@ -7,4 +7,7 @@ setup(name='pisound',
       author_email='sgooding@gmail.com',
       url='https://github.com/sgooding/pisound',
       packages=['pisound'],
+      install_requires=['pydub==0.24.1',
+                        'gpiozero==1.6.2'
+                       ]
      )
